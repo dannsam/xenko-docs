a770a7c6b105987a140d1d42513e81aa9780c8ff
Andrew Shengilevych <ashengilevych@n-ix.com>
2017-01-25 17:43:31 +0200

    ADD: add resizable TOC sidebar

--- template/styles/main.js
+++ template/styles/main.js
@@ -7,4 +7,34 @@ $(function() {
   
   // Captions are not automatically shown anymore by default; uncomment to reenable them
   //showCaptionFromAlt("article img");
+
+  function resizableTOC(){
+    "use srtict"
+    var startSidebarWidth = $('#sidetoggle').width();
+    var contentWidth = $($('.article.grid-right')[0]).width();
+    var contentMargin = + $($('.article.grid-right')[0]).css('marginLeft').split('px')[0];
+    $('#sidetoggle').resizable({
+        containment: ".container.body-content.hide-when-search",
+        handles: 'e',
+        maxWidth: 570,
+        minWidth: startSidebarWidth,
+        resize: function(event, ui){
+            var sidebarSizeDivide = ui.size.width - startSidebarWidth;
+            $($('.article.grid-right')[0]).css({
+                'width' : contentWidth - sidebarSizeDivide,
+                'marginLeft' : contentMargin + sidebarSizeDivide
+            });
+            $($('.sidefilter')[0]).css({
+                'width' : ui.size.width
+            });
+        }
+    });
+  }
+  resizableTOC();
+
+  // $(window).on('resize', function(){
+  //   $('#sidetoggle').resizable( "destroy" );
+  //   resizableTOC();
+  // })
 });
+
