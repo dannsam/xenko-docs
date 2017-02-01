2e4119801c898330764a78673e22b3dbaf94d1f1
Andrew Shengilevych <ashengilevych@n-ix.com>
2017-02-01 17:41:02 +0200

    UPD: change logic for remember the space parameters of sidebar, add new functions

--- template/styles/main.js
+++ template/styles/main.js
@@ -8,71 +8,115 @@ $(function() {
   // Captions are not automatically shown anymore by default; uncomment to reenable them
   //showCaptionFromAlt("article img");
 
-  function resizableTOC(){
-    "use srtict"
-    var sidebarTimer = setInterval(function(){
-      if(localStorage.getItem('sidebarScroll') != null && $('#sidetoc').length > 0){
-        $('#sidetoc').scrollTop(localStorage.getItem('sidebarScroll'));
-        clearInterval(sidebarTimer);
+  // Control the sizes of sidebar and content part (saved, dynamic changed, etc.)
+  function apiDocSizeControl(){
+      "use srtict"
+      function resizableTOC(){      
+      // If object "sizes" saved already
+      if(localStorage.getItem('sizes') != null){
+        // Get the sizes from local storage
+        var sizes = JSON.parse(localStorage.getItem('sizes'));
+        // Set width to sidebar
+        $('#sidetoggle').css('width', sizes.sidebarWidth);
+        // Set width and margin to content part
+        $($('.article.grid-right')[0]).css({
+            'width' : sizes.contentWidth,
+            'marginLeft' : sizes.contentMargin
+        });
+        // Wait, while filter is aviable
+        var filterTimer = setInterval(function(){
+          if($($('.sidefilter')[0]).length > 0){            
+            // Set filter width
+            $($('.sidefilter')[0]).css('width', sizes.sidebarWidth);
+            $($('.sidefilter')[0]).show();
+            // Clear inteval for filter
+            clearInterval(filterTimer);
+          }
+        }, 100);
+        $('.container.body-content.hide-when-search').show();
+      // If object "sizes" don't saved
+      } else {
+        $('.container.body-content.hide-when-search').show();
       }
-    }, 100);   
-    if(localStorage.getItem('sizes') != null){
-      var sizes = JSON.parse(localStorage.getItem('sizes'));
-      $('#sidetoggle').css('width', sizes.sidebarWidth);      
-      $($('.article.grid-right')[0]).css({
-          'width' : sizes.contentWidth,
-          'marginLeft' : sizes.contentMargin
+      // Get started space parameters
+      var startSidebarWidth = $('#sidetoggle').width();
+      var contentWidth = $($('.article.grid-right')[0]).width();
+      var contentMargin = + $($('.article.grid-right')[0]).css('marginLeft').split('px')[0];
+      // Start resizable function
+      $('#sidetoggle').resizable({
+          containment: ".container.body-content.hide-when-search",
+          handles: 'e',
+          maxWidth: 570,
+          minWidth: 140,
+          resize: function(event, ui){
+              // Get difference in sizes between start and end
+              var sidebarSizeDivide = ui.size.width - startSidebarWidth;
+              // Create "Sizes" object
+              var sizes = {
+                sidebarWidth  : ui.size.width,
+                contentWidth  : contentWidth - sidebarSizeDivide,
+                contentMargin : contentMargin + sidebarSizeDivide
+              }
+              // Set sizes for content part
+              $($('.article.grid-right')[0]).css({
+                  'width' : sizes.contentWidth,
+                  'marginLeft' : sizes.contentMargin
+              });
+              // Set sizes for filter
+              $($('.sidefilter')[0]).css('width', sizes.sidebarWidth);
+              localStorage.setItem('sizes', JSON.stringify(sizes))
+          }
       });
-      var filterTimer = setInterval(function(){
-        if($($('.sidefilter')[0]).length > 0){
-          $($('.sidefilter')[0]).css('width', sizes.sidebarWidth);
-          $($('.sidefilter')[0]).show();
-          clearInterval(filterTimer)
-        }
-      }, 100);
-      $('.container.body-content.hide-when-search').show();
+    }
+      // If user on the one of the doc page (API, Manual, ReleaseNotes)
+    if($('#sidetoggle').length > 0){
+      resizableTOC();
+      // If user on start page or somewhere else
     } else {
       $('.container.body-content.hide-when-search').show();
-      var filterTimer = setInterval(function(){
-        if($($('.sidefilter')[0]).length > 0){
-          $($('.sidefilter')[0]).show();
-          clearInterval(filterTimer)
-        }
-      }, 100);
+      localStorage.clear();
     }
-    var startSidebarWidth = $('#sidetoggle').width();
-    var contentWidth = $($('.article.grid-right')[0]).width();
-    var contentMargin = + $($('.article.grid-right')[0]).css('marginLeft').split('px')[0];
-    $('#sidetoggle').resizable({
-        containment: ".container.body-content.hide-when-search",
-        handles: 'e',
-        maxWidth: 570,
-        minWidth: 140,
-        resize: function(event, ui){
-            var sidebarSizeDivide = ui.size.width - startSidebarWidth;
-            var sizes = {
-              sidebarWidth  : ui.size.width,
-              contentWidth  : contentWidth - sidebarSizeDivide,
-              contentMargin : contentMargin + sidebarSizeDivide
-            }
-            $($('.article.grid-right')[0]).css({
-                'width' : sizes.contentWidth,
-                'marginLeft' : sizes.contentMargin
-            });
-            $($('.sidefilter')[0]).css('width', sizes.sidebarWidth);
-            localStorage.setItem('sizes', JSON.stringify(sizes))
-        }
-    });
   }
-  if($('#sidetoggle').length > 0){
-    resizableTOC();
-  } else {
-    $('.container.body-content.hide-when-search').show();
-    localStorage.clear();
-  }
-  $('#sidetoc').on('scroll', function(){
-    localStorage.setItem('sidebarScroll', $(this).scrollTop())
-  });
 
+  function apiSidebarStructureControl(){
+    var tocInterval = setInterval(function(){
+      if($('#toc').length > 0){
+        clearInterval(tocInterval);
+        if(localStorage.getItem('sidebarStructure') != null){
+          getActiveItems();
+          localStorage.clear();
+        }
+        setActiveItems();
+      }
+    }, 100)
+  }
+
+  function setActiveItems(){
+    $('#toc ul li a').on('click', function(){
+      var activeItems = [];
+      $('#toc ul li.in a').each(function(){
+        if($(this).parent().hasClass('in')){
+          activeItems.push($(this).attr('title'));
+        };
+      });
+      var sidebarStructure = {
+        activeItems : activeItems,
+        scroll : $('#sidetoc').scrollTop()
+      }
+      localStorage.setItem('sidebarStructure', JSON.stringify(sidebarStructure));
+    })
+  }
+  function getActiveItems(){
+    var sidebarStructureOut = JSON.parse(localStorage.getItem('sidebarStructure'));
+    var savedActiveItems = sidebarStructureOut.activeItems;
+    for (var i = 0; i < savedActiveItems.length; i++) {
+      $('#toc ul li a[title="' + savedActiveItems[i] + '"]').each(function(){
+        $(this).parent().addClass('in');
+      })
+    }
+    $('#sidetoc').scrollTop(sidebarStructureOut.scroll)
+  }
+  apiDocSizeControl();
+  apiSidebarStructureControl();
 });
 
