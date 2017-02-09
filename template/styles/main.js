0e09aecc79b53432b67d9ef27d2a65bc5e61dc63
Andrew Shengilevych <ashengilevych@n-ix.com>
2017-02-09 14:22:42 +0200

    Added keyup event on submit button for stop submitting form and reloading the page

--- template/styles/main.js
+++ template/styles/main.js
@@ -5,6 +5,14 @@ $(function() {
     });
   };
   
+  $('#search').on('submit', function(e){
+    if (e.preventDefault) { 
+       e.preventDefault();
+    } else {
+       e.returnValue = false; 
+    }
+    $('#search-query').trigger('keyup');
+  })
   // Captions are not automatically shown anymore by default; uncomment to reenable them
   //showCaptionFromAlt("article img");
 });
