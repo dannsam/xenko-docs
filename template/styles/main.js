61da431d8545ec0ee9923468086c87c857237d3f
Andrew Shengilevych <ashengilevych@n-ix.com>
2017-02-17 11:37:53 +0200

    added json files to web.config

--- template/styles/main.js
+++ template/styles/main.js
@@ -5,12 +5,15 @@ $(function() {
     });
   };
   
+  // Function to start searching when user clicked in the search icon
   $('#search').on('submit', function(e){
+    // Remove default events from form
     if (e.preventDefault) { 
        e.preventDefault();
     } else {
        e.returnValue = false; 
     }
+    // Activation keys are pressed, which causes the search function
     $('#search-query').trigger('keyup');
   })
   // Captions are not automatically shown anymore by default; uncomment to reenable them
