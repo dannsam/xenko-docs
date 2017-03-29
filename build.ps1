d9b643c3bf1abb18a2c3a0b6f95281ec88ee127d
Andrew Shengilevych <ashengilevych@n-ix.com>
2017-03-29 12:19:36 +0300

    [Api icons] separation branches v.2

--- build.ps1
+++ build.ps1
@@ -16,6 +16,19 @@ if ($API)
     # Build metadata from C# source
     deps\docfx\docfx.exe metadata | Tee-Object -FilePath build.log -Append
 
+    Write-Host "Generating types of items..."
+
+    # Get all text from api/toc.yml
+    $textYaml = (Get-Content api\toc.yml);
+    # Set start variable for toc files source
+    $folder = "api\"
+    $format = ".yml"
+    # Start copy strings from api/toc.yml to temporary file
+    $isfile2 = Test-Path temporaryTypeToc.yml
+    if($isfile2 -eq 'True'){
+        Remove-Item temporaryTypeToc.yml
+    }
+
     function setTypesToTOCItems($i){
         # if string is uid of item 
         if($textYaml[$i].Contains('- uid:')){
@@ -27,7 +40,7 @@ if ($API)
             for($k = 0; $k -lt $content.length; $k++){
                 if($content[$k].Contains('type:')){
                     if($textYaml[$i][0] -eq ' '){
-                        $typeLine = $content[$k] 
+                        $typeLine = $content[$k]
                         "  $typeLine" | Out-file temporaryTypeToc.yml -append
                     } else {
                         $content[$k] | Out-file temporaryTypeToc.yml -append
@@ -44,6 +57,10 @@ if ($API)
         setTypesToTOCItems($lineCounter);
         $lineCounter++ 
     }
+    
+    '' | Set-Content api\toc.yml
+    (Get-Content temporaryTypeToc.yml) | Set-Content api\toc.yml
+    Remove-Item temporaryTypeToc.yml
     
     # Remove SiliconStudio namespace prefix from TOC
     (Get-Content api\toc.yml).replace('  name: SiliconStudio.', '  name: ') | Set-Content api\toc.yml
