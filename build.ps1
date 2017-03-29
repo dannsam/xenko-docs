232cab20994ef4f9704803b45f32d1c66c3c343e
Andrew Shengilevych <ashengilevych@n-ix.com>
2017-03-29 10:48:18 +0300

    [Api icons] separation branches v.1

--- build.ps1
+++ build.ps1
@@ -15,6 +15,35 @@ if ($API)
     
     # Build metadata from C# source
     deps\docfx\docfx.exe metadata | Tee-Object -FilePath build.log -Append
+
+    function setTypesToTOCItems($i){
+        # if string is uid of item 
+        if($textYaml[$i].Contains('- uid:')){
+            # Copy the uid string
+            $textYaml[$i] | Out-file temporaryTypeToc.yml -append
+            # Open file of this class and find type of the uid
+            $lineEdited = $textYaml[$i].replace('- uid:', '').replace(' ', '').replace('`', '-')
+            $content = (Get-Content "$folder$lineEdited$format");
+            for($k = 0; $k -lt $content.length; $k++){
+                if($content[$k].Contains('type:')){
+                    if($textYaml[$i][0] -eq ' '){
+                        $typeLine = $content[$k] 
+                        "  $typeLine" | Out-file temporaryTypeToc.yml -append
+                    } else {
+                        $content[$k] | Out-file temporaryTypeToc.yml -append
+                    }
+                   break
+                }
+            }
+        } else {
+            $textYaml[$i] | Out-file temporaryTypeToc.yml -append
+        }
+    }
+    $lineCounter = 0;
+    While ($lineCounter -lt $textYaml.length){
+        setTypesToTOCItems($lineCounter);
+        $lineCounter++ 
+    }
     
     # Remove SiliconStudio namespace prefix from TOC
     (Get-Content api\toc.yml).replace('  name: SiliconStudio.', '  name: ') | Set-Content api\toc.yml
